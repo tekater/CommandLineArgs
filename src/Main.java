c66b876a8fd5ffe7317a3a965492ca027ea5825d
tekater <tekateplay1@yandex.ru>
2023-01-20 19:51:57 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        //int i = Integer.parseInt(args[0]);
+        //System.out.println(i);
+
+        // Ограниченное число аргументов
+
+        // Вариант 1
+        //System.out.println(args[0] + " = " + args[1]);
+        //System.out.println(args[2] + " = " + args[3]);
+
+        // Вариант 2
+        //System.out.printf("%s=%s%n%s = %s%n%s=%s",args[0],args[1]);
+
+        //Бесконечное число аргументов
+
+        // Вариант 1
+        /*for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i] + " = " + args[i + 1]);
+        }*/
+
+        // Вариант 2 (более правильный)
+        for (int n = 0, j =1; j < args.length; n+=2, j+= 2) {
+            System.out.println(args[n] + " = " + args[j]);
+        }
+        }
+    public static void main() {
+        System.out.println("No arguments: ");
+
+    }
+}
